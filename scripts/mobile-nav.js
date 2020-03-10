8e2ccb36a8736ac69e0107f0f5b4a82dd99bfab5
ratparty <sgbourbon@gmail.com>
2020-03-10 17:46:41 +0000

    Reformat mobile nav menu
    Make the menu bar expand full width after landing image, with site 
    title. Centre open/close icons. Smooth animation timing.

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -4,16 +4,36 @@ function topnav() {
   var nav = document.getElementById("mobileNavMenu");
   var open = document.getElementById("open");
   var close = document.getElementById("close");
+  var navBar = document.getElementById("mobileNavBar");
   if (nav.style.maxHeight === "15rem") {
     nav.style.maxHeight = "0";
     nav.style.padding = "0 10px";
+    nav.style.maxWidth = "3rem";
+        nav.style.transition = "0.7s ease-in-out, max-width .7s .5s";
+        nav.style.borderBottomLeftRadius = "0";
     open.style.display = "inline-block";
     close.style.display = "none";
+            nav.style.width = "auto";
+              navBar.classList.remove("nav-bar--open");
+              navBar.classList.add("nav-bar--closed");
+            navBar.style.transition ="0.7s ease-in-out, border-bottom-left-radius .3s .7s";
+
+
   } else {
     nav.style.maxHeight = "15rem";
    nav.style.padding = ".3em 10px 1em";
     open.style.display = "none";
     close.style.display = "block";
+        nav.style.maxWidth = "10rem";
+                nav.style.borderBottomLeftRadius = "15px";
+    navBar.style.borderBottomLeftRadius = "0";
+    nav.style.width = "auto";
+    nav.style.transition = "0.7s ease-in-out .25s, max-width .0s .0s";
+    navBar.style.transition ="0.7s ease-in-out, border-bottom-left-radius .3s";
+    navBar.classList.add("nav-bar--open");
+    navBar.classList.remove("nav-bar--closed");
+
+
   }
 }
 
--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,10 @@ const topNavHeader = document.getElementById("topNavHeader");
 const topNavTitle = document.getElementById("topNavTitle");
 const colourBlock = document.getElementById("colourBlock");
 const mainNavList = document.getElementById("mainNavList");
+const mobileNavBar = document.getElementById("mobileNavBar");
+const mobileTitle = document.getElementById("mobileTitle");
+const navIcons = document.getElementById("navIcons");
 
-window.onscroll = function() {navBar()};
 
 function navBar() {
   if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
@@ -17,6 +19,13 @@ function navBar() {
     topNavTitle.classList.add("top-nav__title--scrolled");
     colourBlock.classList.add("colour-block--scrolled");
     mainNavList.classList.add("main-nav__list--scrolled");
+    mobileNav.classList.add("mobile-nav--scrolled");
+    mobileNavBar.classList.add("mobile-nav__bar--scrolled");
+    mobileTitle.style.visibility = "visible";
+    mobileTitle.style.maxHeight = "5rem";
+    mobileTitle.style.opacity = "1";
+    mobileTitle.style.transition = "opacity .7s .5s, max-height .0s .4s";
+    mobileNavBar.style.borderBottomLeftRadius = " 0";
   }
   else {
     topNav.classList.remove("top-nav--scrolled");
@@ -24,5 +33,16 @@ function navBar() {
     topNavTitle.classList.remove("top-nav__title--scrolled");
     colourBlock.classList.remove("colour-block--scrolled");
         mainNavList.classList.remove("main-nav__list--scrolled");
+            mobileNav.classList.remove("mobile-nav--scrolled");
+                mobileTitle.style.visibility = "hidden";
+                mobileTitle.style.maxHeight = "0rem";
+                    mobileTitle.style.opacity = "0";
+                    mobileTitle.style.transition = "opacity .7s .5s, max-height .0s .0s";
+
+
+  }
+}
+
+
   }
 }
