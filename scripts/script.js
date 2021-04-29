a7ed25fe21310366a7709af45da1af0a10edf6f5
Sam <sgbourbon@gmail.com>
2021-04-29 02:35:20 +0100

    update mobile menu to close after clicking link

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,26 @@ let mainNav = document.getElementById("mainNav");
 let navButton = document.getElementById("navButton");
 let iconOpen = document.getElementById("iconOpen");
 let iconClose = document.getElementById("iconClose");
+let navLinks = document.querySelectorAll('.nav-link');
 
 window.onscroll = function() {navBar()};
 
 navButton.addEventListener('click', toggleMenu);
 
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', closeMenuOnSelection);
+}
+
 function toggleMenu() {
   iconOpen.classList.toggle('main-nav__icon--visible');
   iconClose.classList.toggle('main-nav__icon--visible');
   mainNav.classList.toggle('main-nav--open');
+}
+
+function closeMenuOnSelection() {
+  if (mainNav.classList.contains('main-nav--open') {
+    mainNav.classList.remove('main-nav--open');
+  })
 }
 
 
