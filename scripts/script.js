24fdde6808b51dc62fb6dc2f6ef02d69c4ef14fd
Sam <sgbourbon@gmail.com>
2021-04-29 02:22:59 +0100

    update site

--- scripts/script.js
+++ scripts/script.js
@@ -1,45 +1,19 @@
 const h = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
 
-const topNav = document.getElementById("topNav");
-const topNavHeader = document.getElementById("topNavHeader");
-const topNavTitle = document.getElementById("topNavTitle");
-const colourBlock = document.getElementById("colourBlock");
-const mainNavList = document.getElementById("mainNavList");
-const mobileNavBar = document.getElementById("mobileNavBar");
-const mobileTitle = document.getElementById("mobileTitle");
-const navIcons = document.getElementById("navIcons");
+let mainNav = document.getElementById("mainNav");
+let navButton = document.getElementById("navButton");
+let iconOpen = document.getElementById("iconOpen");
+let iconClose = document.getElementById("iconClose");
 
 window.onscroll = function() {navBar()};
 
-function navBar() {
-  if (document.body.scrollTop > h || document.documentElement.scrollTop > h) {
-    topNav.classList.add("top-nav--scrolled");
-    topNavHeader.classList.add("top-nav__header--scrolled");
-    topNavTitle.classList.add("top-nav__title--scrolled");
-    colourBlock.classList.add("colour-block--scrolled");
-    mainNavList.classList.add("main-nav__list--scrolled");
-    mobileNav.classList.add("mobile-nav--scrolled");
-    mobileNavBar.classList.add("mobile-nav__bar--scrolled");
-    mobileTitle.style.visibility = "visible";
-    mobileTitle.style.maxHeight = "5rem";
-    mobileTitle.style.opacity = "1";
-    mobileTitle.style.transition = "opacity .7s .5s, max-height .0s .4s";
-    mobileNavBar.style.borderBottomLeftRadius = " 0";
-  }
-  else {
-    topNav.classList.remove("top-nav--scrolled");
-    topNavHeader.classList.remove("top-nav__header--scrolled");
-    topNavTitle.classList.remove("top-nav__title--scrolled");
-    colourBlock.classList.remove("colour-block--scrolled");
-    mainNavList.classList.remove("main-nav__list--scrolled");
-    mobileNav.classList.remove("mobile-nav--scrolled");
-    mobileNavBar.classList.remove("mobile-nav__bar--scrolled");
-    mobileTitle.style.visibility = "hidden";
-    mobileTitle.style.maxHeight = "0rem";
-    mobileTitle.style.opacity = "0";
-    mobileTitle.style.transition = "opacity .7s .5s, max-height .0s .0s";
-    mobileNavBar.style.borderBottomLeftRadius = null;
-  }
+navButton.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  iconOpen.classList.toggle('main-nav__icon--visible');
+  iconClose.classList.toggle('main-nav__icon--visible');
+  mainNav.classList.toggle('main-nav--open');
 }
+
+
+
